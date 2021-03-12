d04926be36e1654bd6ce0af1fa0c67a36c65c3c5
ganzhiqin <ganzhiqin@sinoiov.com>
2021-03-12 16:56:12 +0800

    init

---
+++ src/main/java/Algorithm/base/base34/SolutionTest.java
@@ -0,0 +1,9 @@
+package Algorithm.base.base34;
+
+public class SolutionTest {
+    public static void main(String[] args) {
+        String s = "skljdsgwev";
+        String t = "dsg";
+        System.out.println(new KMP().indexOf(s, t));
+    }
+}
